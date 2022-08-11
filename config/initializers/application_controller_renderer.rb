23f5d4359e4da0bc518c7f3ffd08182112aca980
machu0610 <88845370+machu0610@users.noreply.github.com>
2022-08-11 15:02:00 +0900

    一覧画面の表示

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9054c30346eacf49af5af901c481ca5054e79cb229b2c85a876ac8872e404bca370c75323b541e69cd899db73752b2ca83eac050ae03e46321e7378d875e9c75'
