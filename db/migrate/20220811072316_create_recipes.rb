5051dac6d5d627b37de0767e7228c211ff2ed6d6
machu0610 <88845370+machu0610@users.noreply.github.com>
2022-08-11 16:25:27 +0900

    db作成

--- db/migrate/20220811072316_create_recipes.rb
+++ db/migrate/20220811072316_create_recipes.rb
@@ -2,7 +2,7 @@ class CreateRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :recipes do |t|
       t.integer :user_id
-      t.strig :title
+      t.string :title
       t.text :body
       t.string :image_id
 
